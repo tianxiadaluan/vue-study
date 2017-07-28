b6500ab5cf82c229e46783a9a69637ea2c4da329
wangshuai <712366400@qq.com>
2017-07-28 23:57:41 +0800

    基础环境需求上传

---
+++ elementui-admin-bata/src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="app">
+    <el-row :gutter="10">
+      <el-col :span="6">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+    </el-row>
+    <el-row :gutter="10">
+      <el-col :span="6">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+    </el-row>
+    <div class="block">
+      <span class="demonstration">页数较少时的效果</span>
+      <el-pagination layout="prev, pager, next" :total="50">
+      </el-pagination>
+    </div>
+  </div>
+</template>
+
+<script>
+// import {Loading} from 'element-ui'
+export default {
+  name: 'app',
+  components: {},
+  mounted () { }
+}
+</script>
+
+<style>
+.el-row {
+  margin-bottom: 20px;
+}
+
+.el-row:last-child {
+  margin-bottom: 0;
+}
+
+.el-col {
+  border-radius: 4px;
+}
+
+.bg-purple-dark {
+  background: #99a9bf;
+}
+
+.bg-purple {
+  background: #d3dce6;
+}
+
+.bg-purple-light {
+  background: #e5e9f2;
+}
+
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+</style>
