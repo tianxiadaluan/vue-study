98f25ca635c48ae670b38033f2d4dfd2d7e0bac1
wangshuai <712366400@qq.com>
2017-08-03 00:21:48 +0800

    基础框架设置 待完善

--- elementui-admin-bata/.eslintrc.js
+++ elementui-admin-bata/.eslintrc.js
@@ -22,6 +22,8 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // 语句强制分号结尾
+    "semi": [2, "always"]
   }
 }
--- elementui-admin-bata/src/App.vue
+++ elementui-admin-bata/src/App.vue
@@ -1,19 +1,64 @@
 <template>
   <div id="app">
-    {{this.$store.state.count}}
-    <router-view></router-view>
+    <v-header></v-header>
+    <v-slider></v-slider>
+    <router-view id="content"></router-view>
   </div>
 </template>
 <script>
+import header from '@/components/public/header';
+import slider from '@/components/public/slider';
 export default {
   name: 'app',
   data () {
-    return {};
+    return {
+      activeIndex: '1',
+      activeIndex2: '1'
+    };
+  },
+  components: {
+    'v-header': header,
+    'v-slider': slider
   },
   methods: {
     addgo () {
       this.$store.commit('increment');
+    },
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath);
     }
   }
 };
 </script>
+<<style>
+html,body{
+  margin: 0;
+  padding: 0;
+  height:100%;
+}
+#header{
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  height: 60px;
+  background: #ccc;
+}
+#slider{
+  position: absolute;
+  top: 60px;
+  bottom: 0;
+  left: 0;
+  width:200px;
+  background:#ddd;
+}
+#content{
+  position: absolute;
+  top: 60px;
+  right: 0;
+  bottom: 0;
+  left: 200px;
+  background:#bbb;
+}
+</style>
+
