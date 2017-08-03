971bf3542f9b8e0f54b77c233ffd2cf83d482433
wangshuai <712366400@qq.com>
2017-08-04 00:22:55 +0800

    修改左侧导航为router模式

--- elementui-admin-bata/src/components/public/slider.vue
+++ elementui-admin-bata/src/components/public/slider.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="slider">
-    <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+    <el-menu default-active="1-4-1" class="el-menu-vertical-demo" :router="true" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
       <el-submenu index="1">
         <template slot="title">
           <i class="el-icon-message"></i>
